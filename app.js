3736b02d3c5ac3e973e98aec8ad5c028eadc63b4
Chase K <chasekmbll@gmail.com>
2025-06-12 19:19:07 -0500

    product and review routes, also made an app file

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+export default app;
+
+export const verifyToken = () => {
+	const authHeader = req.headers["Authorization"];
+	const token = authHeader.split(" ")[1];
+	const decoded = jwt.verify(token, process.env.JWT_SECRET);
+	req.user = decoded;
+	next();
+};
+
+app.use((err, req, res, next) => {
+	console.error(err);
+	res.status(500).send("Sorry! Something went wrong.");
+});
